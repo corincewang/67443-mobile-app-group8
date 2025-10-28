29065f78a04abcd4d0a95ba3ba50132da47d63ac
corinceSub <hanwang3@andrew.cmu.edu>
2025-10-28 11:40:08 -0400

    Reinitialize Pawse project

---
+++ Pawse/PawseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawseApp.swift
+//  Pawse
+//
+//  Created by Wang Han on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PawseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawseTests/PawseTests.swift
@@ -0,0 +1,16 @@
+//
+//  PawseTests.swift
+//  PawseTests
+//
+//  Created by Wang Han on 10/28/25.
+//
+
+import Testing
+
+struct PawseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
